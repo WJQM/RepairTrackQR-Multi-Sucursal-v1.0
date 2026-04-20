4fa9ed8541a38926b7690e456dd4c5a20784d442
WJQM <wiljamesquenta4@gmail.com>
2026-04-20 00:21:48 -0400

    v1.0 limpio - migración consolidada

--- prisma/migrations/20260415_add_equipment_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable - Add new fields to Equipment
-ALTER TABLE "Equipment" ADD COLUMN IF NOT EXISTS "storage2" TEXT;
-ALTER TABLE "Equipment" ADD COLUMN IF NOT EXISTS "cabinet" TEXT;
-ALTER TABLE "Equipment" ADD COLUMN IF NOT EXISTS "powerSupply" TEXT;
-ALTER TABLE "Equipment" ADD COLUMN IF NOT EXISTS "accessories" TEXT;
